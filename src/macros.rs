2c7cf3bd6602d8027dc7f3a45a263121c398ca64
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2025-04-10 19:57:07 -0600

    allow(unused_macros)

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,7 @@
 //! Various macros. The `u` prefixed macros such as `uassert!` avoid performing any checks if the `unsafe` feature is active.
 
+#![allow(unused_macros)]
+
 macro_rules! trace {
     (file $fmt:expr, $($name:expr),* ; $val:expr $(,)?) => {
         #[cfg(feature = "trace")]
